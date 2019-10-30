578957500676fdd82d2a933f8888b28d1bbe488a
Austin T <austin.taranto@gmail.com>
2019-10-30 17:01:41 -0400

    Updates

---
+++ precache-manifest.a3d40c6b057d7867df48ca8ba60cbd39.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6e4ce32b8f7f5f5a7261e678ba3456d",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "fb9c40d4c3823aba2b56",
+    "url": "/todo-list/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "03083507c0fcc3cc8218",
+    "url": "/todo-list/static/js/2.522ec9fe.chunk.js"
+  },
+  {
+    "revision": "fb9c40d4c3823aba2b56",
+    "url": "/todo-list/static/js/main.bbb47a6e.chunk.js"
+  },
+  {
+    "revision": "da715ee0b4f01398cebe",
+    "url": "/todo-list/static/js/runtime-main.14b3a6b5.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/todo-list/static/media/logo.25bf045c.svg"
+  }
+]);
