cb1eca6b7035ec6daaec591fdbe7053f948c87eb
Austin T <austin.taranto@gmail.com>
2019-10-30 17:27:25 -0400

    added this state and empty array for todos

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
 import React from 'react';
-import TodoList from './TodoList';
-import TodoItem from './TodoItem';
+import TodoList from './todoList';
+import TodoItem from './todoItem';
 // import logo from './logo.svg';
 // import './App.css';
 
 class App extends React.Component {
 
+  constructor() {
+    //parents constructor
+    super();
+    //object
+    this.state = {
+      //array
+      todos: []
+    };
+  }
+
   redner() {
-    return ()
+    return (
+    <div>
+      <TodoList></TodoList>
+      <TodoItem></TodoItem>
+    </div>
+    );
+  }
+
+  //local storage function
+  // put into DOM
+  componenetDidMount = () => {
+    const todos = localStorage.getItem('todos');
+    if(todos) {
+      console.log('has todos', todos);
+    } else {
+      console.log('no todos');
+    }
   }
 
 }
